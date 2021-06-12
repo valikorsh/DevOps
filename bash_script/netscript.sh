c7dfb81fcbfcc484655487c4052e23e883481ea9
valik <valik1985@list.ru>
2021-06-12 22:51:12 +0300

    modify script

--- bash_script/netscript.sh
+++ bash_script/netscript.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
-#input name or pid process
-echo "This script running with root privileges"
-#read -p "Execute with sudo ?":yes
-#input name or ID process
-read -p "Input name or ID process:" procid
-echo `netstat -tunapl | awk -v procid=$procid '{print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done`
+echo "Attention !!! It is recommended \
+to run this script from root accounts"
+#Would you like to do it now? \
+#Please insert(y/n): "
+#read quest
+#if [$quest != 'y']
+#	then 
+#	echo ""
+#fi
+#input name or ID process 
+echo "Input name or ID process: "
+read name
+#input number of lines to output
+echo "Input number lines: "
+read line
+if [ "$name" = "" ]
+	then	
+		echo "Error:not input not found"
+		exit 1
+	else
+	netstt=`netstat -tunapl | awk -v name=$name '$0 ~ name {print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n$line |grep -oP '(\d+\.){3}\d+' | while read IP; do whois $IP | awk -F':' '/^Organization/ {print $2}'; done`
+	echo $netstt
+fi
